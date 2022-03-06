cb8afdd9eb8e49e26e3860d82177fd56d0dc91c5
Roman <ackhtariev@gmail.com>
2022-03-06 13:37:44 -0800

    abstraction for connection

---
+++ node/connection.go
@@ -0,0 +1,61 @@
+package node
+
+import (
+	"context"
+	"fmt"
+	"strconv"
+
+	grpctypes "github.com/cosmos/cosmos-sdk/types/grpc"
+
+	"google.golang.org/grpc"
+	"google.golang.org/grpc/credentials/insecure"
+	"google.golang.org/grpc/metadata"
+)
+
+type Connection struct {
+	grpcConn *grpc.ClientConn
+}
+
+func NewConnection() (*Connection, error) {
+	grpcConn, err := grpc.Dial("104.248.92.191:9090",grpc.WithTransportCredentials(insecure.NewCredentials()))
+	if err != nil {
+		return nil, err
+	}
+	return &Connection{
+		grpcConn: grpcConn,
+	}, nil
+}
+
+func (c *Connection) InvokeClient(height int, makeRequestCb func (*grpc.ClientConn,  context.Context, *metadata.MD) (interface{}, error)) (interface{}, int, error) {
+	var header metadata.MD
+	ctxWithHeight := metadata.AppendToOutgoingContext(context.Background(), grpctypes.GRPCBlockHeightHeader, strconv.Itoa(height))
+
+	resp, err := makeRequestCb(c.grpcConn, ctxWithHeight, &header)
+	if err != nil {
+		return nil, 0, err
+	}
+
+	blockHeightUnparsed := header.Get(grpctypes.GRPCBlockHeightHeader)
+	if len(blockHeightUnparsed) > 1 {
+		return nil, 0, fmt.Errorf("invalid block height header: %s", blockHeightUnparsed)
+	}
+
+	blockHeight, err := strconv.Atoi(blockHeightUnparsed[0])
+	if err != nil {
+		return nil, 0, fmt.Errorf("error parsing block height header: %s", blockHeightUnparsed)
+	}
+
+	return resp, blockHeight, nil
+}
+
+func (c *Connection) Invoke(method string, reply interface{}) error {
+	if err := c.grpcConn.Invoke(context.Background(), method, nil, reply, grpc.EmptyCallOption{}); err != nil {
+		return err
+	}
+	return nil
+}
+
+func (c *Connection) Close() error {
+	return c.grpcConn.Close()
+}
+
--- query/query.go
+++ query/query.go
@@ -3,52 +3,42 @@ package query
 import (
 	"context"
 	"fmt"
-	"strconv"
+
+	"github.com/p0mvn/perf-osmo/v2/node"
 
 	"github.com/cosmos/cosmos-sdk/client/grpc/tmservice"
-	grpctypes "github.com/cosmos/cosmos-sdk/types/grpc"
 	epochs "github.com/osmosis-labs/osmosis/v7/x/epochs/types"
 	lockup "github.com/osmosis-labs/osmosis/v7/x/lockup/types"
 	"google.golang.org/grpc"
-	"google.golang.org/grpc/credentials/insecure"
 	"google.golang.org/grpc/metadata"
 )
 
 func Start() error {
-
-	// Create a connection to the gRPC server.
-	grpcTendermintConn, err := grpc.Dial("104.248.92.191:9090",grpc.WithTransportCredentials(insecure.NewCredentials()))
+	conn, err := node.NewConnection()
 	if err != nil {
 		return err
 	}
-	defer grpcTendermintConn.Close()
-	
+
+	defer conn.Close()
 
 	reply := &tmservice.GetNodeInfoResponse{}
-	if err := grpcTendermintConn.Invoke(context.Background(), "/cosmos.base.tendermint.v1beta1.Service/GetNodeInfo", nil, reply, grpc.EmptyCallOption{}); err != nil {
+	if err := conn.Invoke("/cosmos.base.tendermint.v1beta1.Service/GetNodeInfo", reply); err != nil {
 		return err
 	}
 	fmt.Println(reply)
 
-    // // Create a connection to the gRPC server.
-    // grpcConn, err := grpc.Dial("104.248.92.191:9090",grpc.WithTransportCredentials(insecure.NewCredentials()))
-	// if err != nil {
-	// 	return err
-	// }
-    // defer grpcConn.Close()
+	epochsResp, _, err := conn.InvokeClient(3335437, currEpochRequest)
+	if err != nil {
+		return err
+	}
+	fmt.Println(epochsResp)
 
-	// epochsResp, _, err := makeRequest(grpcConn, 3335437, currEpochRequest)
-	// if err != nil {
-	// 	return err
-	// }
-	// fmt.Println(epochsResp)
-
-	// lockupResp, _, err := makeRequest(grpcConn, 3335437, getLockupModuleBalance)
-	// if err != nil {
-	// 	return err
-	// }
-	// fmt.Println(lockupResp)
-	// fmt.Println(err)
+	lockupResp, _, err := conn.InvokeClient(3335437, getLockupModuleBalance)
+	if err != nil {
+		return err
+	}
+	fmt.Println(lockupResp)
+	fmt.Println(err)
 
     return nil
 }
@@ -68,26 +58,4 @@ func currEpochRequest(grpcConn *grpc.ClientConn, ctx context.Context, header *me
 		Identifier: "day",
 	}
 	return epochsClient.CurrentEpoch(ctx, req, grpc.Header(header))
-}
-
-func makeRequest(grpcConn *grpc.ClientConn, height int, makeRequestCb func (*grpc.ClientConn,  context.Context, *metadata.MD) (interface{}, error)) (interface{}, int, error) {
-	var header metadata.MD
-	ctxWithHeight := metadata.AppendToOutgoingContext(context.Background(), grpctypes.GRPCBlockHeightHeader, strconv.Itoa(height))
-
-	resp, err := makeRequestCb(grpcConn, ctxWithHeight, &header)
-	if err != nil {
-		return nil, 0, err
-	}
-
-	blockHeightUnparsed := header.Get(grpctypes.GRPCBlockHeightHeader)
-	if len(blockHeightUnparsed) > 1 {
-		return nil, 0, fmt.Errorf("invalid block height header: %s", blockHeightUnparsed)
-	}
-
-	blockHeight, err := strconv.Atoi(blockHeightUnparsed[0])
-	if err != nil {
-		return nil, 0, fmt.Errorf("error parsing block height header: %s", blockHeightUnparsed)
-	}
-
-	return resp, blockHeight, nil
 }
