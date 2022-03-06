74d78fad9f8b7c150ce99b3ae7d5f93ee1250d85
Roman <ackhtariev@gmail.com>
2022-03-06 15:26:39 -0800

    implement RegisterCalls

--- x/epochs/module.go
+++ x/epochs/module.go
@@ -1,19 +1,25 @@
 package epochs
 
-import "github.com/p0mvn/perf-osmo/v2/perf/module"
+import (
+	"context"
+
+	"github.com/p0mvn/perf-osmo/v2/perf/module"
+	"google.golang.org/grpc"
+	"google.golang.org/grpc/metadata"
+)
 
 var _ module.PerfModule                 = (*EpochsModule)(nil)
 
 type EpochsModule struct {
-
+	calls []func(grpcConn *grpc.ClientConn, ctx context.Context, header *metadata.MD) (interface{}, error)
 }
 
 func NewPerfModule() module.PerfModule {
 	return &EpochsModule{}
 }
 
-func (*EpochsModule) RegisterCalls() {
-
+func (em *EpochsModule) RegisterCalls() {
+	em.calls = append(em.calls, CurrEpochRequest)
 }
 
 func (*EpochsModule) CallRandom() error {
--- x/lockup/module.go
+++ x/lockup/module.go
@@ -1,19 +1,25 @@
 package lockup
 
-import "github.com/p0mvn/perf-osmo/v2/perf/module"
+import (
+	"context"
+
+	"github.com/p0mvn/perf-osmo/v2/perf/module"
+	"google.golang.org/grpc"
+	"google.golang.org/grpc/metadata"
+)
 
 var _ module.PerfModule                 = (*LockupModule)(nil)
 
 type LockupModule struct {
-
+	calls []func(grpcConn *grpc.ClientConn, ctx context.Context, header *metadata.MD) (interface{}, error)
 }
 
 func NewPerfModule() module.PerfModule {
 	return &LockupModule{}
 }
 
-func (*LockupModule) RegisterCalls() {
-
+func (lm *LockupModule) RegisterCalls() {
+	lm.calls = append(lm.calls, GetLockupModuleBalance)
 }
 
 func (*LockupModule) CallRandom() error {
